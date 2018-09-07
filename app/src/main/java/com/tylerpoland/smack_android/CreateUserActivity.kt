06e994817f2a06845f761b015a20a52efbf1087f
Tyler Poland <poland.tyler@gmail.com>
2018-09-06 20:17:05 -0600

    user avatar/color generation + fixes + cleanup

--- app/src/main/java/com/tylerpoland/smack_android/CreateUserActivity.kt
+++ app/src/main/java/com/tylerpoland/smack_android/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.tylerpoland.smack_android
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    private var userAvatar = "profiledefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +21,31 @@ class CreateUserActivity : AppCompatActivity() {
 
     }
 
-    fun createTapGenerateUserButtonClicked(view: View) {
+    fun createGenerateUserButtonClicked(view: View) {
+        val random = Random()
+        val color = random.nextInt(2) // upper bound, not inclusive
+        val avatar = random.nextInt(28)
 
+        userAvatar = if (color == 0) {
+            "light$avatar"
+        } else {
+            "dark$avatar"
+        }
+        val resourceID = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createUserAvatarImageButton.setImageResource(resourceID)
+    }
+
+    fun generateNewColorButtonClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createUserAvatarImageButton.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+        avatarColor = "[$savedR, $savedG, $savedB]" // set avatarColor to something the server can consume (iOS & macOS require RGB values < 1)
     }
 }
